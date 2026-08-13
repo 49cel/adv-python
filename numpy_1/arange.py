7077d8be9b88f9d05e6a04d4384fb4bbf0ca46e9
49cel <pranavponnada07@gmail.com>
2026-08-13 10:12:50 +0530

    addition: info about arange function

--- numpy_1/arange.py
+++ numpy_1/arange.py
@@ -4,3 +4,12 @@ import numpy as np
 
 a = np.arange(1, 10)
 print(a)
+
+x = np.arange(10.4) # default step is taken as 1, prints floating point values from 1 to 10
+print(x)
+
+y = np.arange(0.5, 10, 0.5) # prints values from 0.5 to 10 (not including 10), with a step of 0.5
+print(y)
+
+z = np.arange(0.5, 10, 0.5, int) # same thing but only prints the integral part, drops fractional part
+print(z)
